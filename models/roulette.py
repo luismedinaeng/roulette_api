36fd1775bc41fe8369b1a0d85cd0f07c01a546ad
luismedinaeng <malf95@hotmail.com>
2020-06-03 02:33:08 -0500

    [Fix]Syntax errors at Roulette class

--- models/roulette.py
+++ models/roulette.py
@@ -9,18 +9,18 @@ class Roulette():
 	CLOSE = 1
 	WITHOUT_RESULT = {} 
 
-	def __init__(self, id):
+	def __init__(self):
 		self.id = id if id else str(uuid.uuid4())
-		self.status = OPEN
-		self.bets = []
-		self.__result = WITHOUT_RESULT
+		self.status = Roulette.CLOSE
+		self.__bets = []
+		self.__result = Roulette.WITHOUT_RESULT
 
 	@property
 	def id(self):
 		return self.__id
 	
 	@id.setter
-	def if(self, id):
+	def id(self, id):
 		self.__id = id
 
 	@property
@@ -33,7 +33,11 @@ class Roulette():
 
 	@property
 	def bets(self):
-		return bets
+		return self.__bets
+
+	@property
+	def result(self):
+		return self.__result
 
 	def add_bet(self, new_bet):
 		self.__bets.append(new_bet)
@@ -48,13 +52,13 @@ class Roulette():
 		self.__bets.clear()
 
 	def open(self):
-		if self.status = CLOSE:
+		if self.status == Roulette.CLOSE:
 			self.clean()
-			self.result = WITHOUT_RESULT
-			self.status = OPEN
+			self.result = Roulette.WITHOUT_RESULT
+			self.status = Roulette.OPEN
 			
 	def close(self):
-		if self.status = OPEN:
+		if self.status == Roulette.OPEN:
 			number = random.randrange(Bet.MIN_TOKEN, Bet.MAX_TOKEN)
 			color = get_color_of_result(number)
 			self.__result = {'number': number, 'color': color}
@@ -63,7 +67,7 @@ class Roulette():
 	def get_color_of_result(result):
 		black_numbers = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
 		red_numbers = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
-		if number in black_numbers.
+		if number in black_numbers:
 			color = Bet.BLACK_TOKEN
 		else:
 			color = Bet.RED_TOKEN
