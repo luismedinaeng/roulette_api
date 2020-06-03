2ff1a8c935288d88916ff2d4b160b8598c8a96c2
luismedinaeng <malf95@hotmail.com>
2020-06-03 09:43:01 -0500

    [Add]Endpoints of bet and opening and closing roulettes

--- api/app.py
+++ api/app.py
@@ -28,17 +28,60 @@ def get_roulettes():
 	content = jsonify(list_roulettes)
 	return make_response(content, 200)
 
-@app.route('/ruletas', methods=['POST'])
+@app.route('/new_roulette')
+@app.route('/roulettes', methods=['POST'])
 def new_roulette():
 	instance = Roulette()
 	instance.save()
 	print
-	content = jsonify({'id_ruleta': instance.id})
+	content = jsonify({'id_roulette': instance.id})
 	return make_response(content, 200)
-		
 
+@app.route('/roulettes/<id>/open', methods=['GET'])	
+def open_roulette(id):
+	roulette = models.casino.get(Roulette, id)
+	roulette.open()
+	content = jsonify({'estado_roulette': roulette.status})
+	return make_response(content, 200)
+	
+@app.route('/roulettes/<id>/close', methods=['GET'])	
+def close_roulette(id):
+	roulette = models.casino.get(Roulette, id)
+	list_bets = []
+	for bet in roulette.bets():
+		list_bets.append(bet)
+	content = jsonify(list_bets)
+	return make_response(content, 200)
 
+def check_json_bet(request):
+	description = ""
+	if not request.get_json():
+	   description = "Not a JSON"
+	elif 'user_id' not in request.get_json():
+		description = "missing user_id"
+	elif 'token' not in request.get_json():
+		description = "Missing token"
+	elif 'value' not in request.get_json():
+		description = "Missing bet value"
+	return description
 
+@app.route('/roulettes/<id>/bet', methods=['POST'])
+def create_bet(id):
+	roulette = models.casino.get(Roulette, id)
+	if roulette.status == Roulette.CLOSE:
+		content = jsonify({'error': 'Closed roullette'})
+		return make_response(content, 403)
+	description = check_json_bet(request)
+	if description != "":
+		content = jsonify({'error': description})
+		return make_response(content, 400)
+	data = request.get_json()
+	data['roulette_id'] = id
+	try:
+		bet = Bet(**data)
+		bet.save()
+	except:
+		abort(400, description="Too many args")
 
 if __name__ == '__main__':
 	host = '0.0.0.0'
